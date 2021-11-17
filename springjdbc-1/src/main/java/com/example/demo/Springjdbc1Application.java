646810da18f7a701fea4a7ef72a4fb16bffb580e
Shaik Tasleem kousar <shaiktasleem989@gmail.com>
2021-11-17 08:53:35 +0530

    springboot initial commit

---
+++ springjdbc-1/src/main/java/com/example/demo/Springjdbc1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springjdbc1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springjdbc1Application.class, args);//repository2 master
+	}
+
+}
